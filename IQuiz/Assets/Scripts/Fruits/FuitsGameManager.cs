6c8905cfa264eade8f42d397a56dbba65d52fb2b
Adrian Caamino <71427464+dopelemmon@users.noreply.github.com>
2024-01-05 10:38:27 +0800

    modifying fruits game (NEED BUT TESTING IN TIME)

--- IQuiz/Assets/Scripts/Fruits/FuitsGameManager.cs
+++ IQuiz/Assets/Scripts/Fruits/FuitsGameManager.cs
@@ -4,22 +4,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Unity.Collections.LowLevel.Unsafe;
 
 namespace IQuiz
 {
     public class FruitsGameManager : MonoBehaviour
     {
         // Public Variables
+        [Header("UI Elements")]
         public GameObject[] fruitsPrefab; // Array of fruit prefabs
         public GameObject buttonPrefab; // Reference to the button prefab
         public Transform fruitParent; // Parent transform for fruit instantiation
         public Transform buttonsParent; // Parent transform for buttons
+        public TMP_Text nameText;
+        public TMP_Text playerScoreText;
+        [Space]
+
+        [Header("Other Script Reference")]
+        public SandClock sandClock;
+        [Space]
+
+        [Header("Audio")]
+        public AudioSource timerSounds;
+        [Space]
+
+        [Header("Time")]
+        public float gameTimer;
+
+        [Header("Player Stats")]
+        public int playerScore;
 
         // Private Variables
         GameObject fruitsGameObj; // Reference to the spawned fruit object
         string fruitName; // Name of the fruit
         StringShuffler shuffler = new StringShuffler(); // Instance of the StringShuffler class to shuffle strings
         List<GameObject> buttonList = new List<GameObject>(); // List to store created buttons
+
+        [SerializeField] private Button lastButtonClicked; // Reference to the last clicked button
+
+        char clickedLetter;
 
         string userInput = ""; // User's input for spelling the fruit name
 
@@ -28,21 +51,23 @@ namespace IQuiz
         // Start is called before the first frame update
         void Start()
         {
+            sandClock.onRoundEnd += OnRoundEnd;
             // For example purposes, assume fruitName is obtained from the instantiated fruit object
             fruitName = "Apple"; // Replace this with the actual fruit name from your game
 
             // Shuffle the fruit name to create a jumbled version for buttons
             string shuffledName = shuffler.ShuffleString(fruitName);
+
+            SpawnQuestion();
+            playerScore = 0;
         }
 
         // Update is called once per frame
         void Update()
         {
-            // Check for user input to spawn fruits and create buttons
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SpawnQuestion();
-            }
+            UpdateTime();
+
+            playerScoreText.text = $"Score: {playerScore}";
         }
 
         #endregion
@@ -56,23 +81,47 @@ namespace IQuiz
             {
                 // Instantiate buttons and set their properties
                 GameObject button = Instantiate(buttonPrefab, buttonsParent);
-                button.GetComponentInChildren<TMP_Text>().text = letter.ToString();
-                button.GetComponent<Button>().onClick.AddListener(() => OnButtonClick(letter));
                 buttonList.Add(button);
+                button.GetComponentInChildren<TMP_Text>().text = letter.ToString();
+                button.GetComponent<Button>().onClick.AddListener(() => OnButtonClick(letter, lastButtonClicked));
+
             }
         }
 
         // Handle button click event
-        void OnButtonClick(char clickedLetter)
+        void OnButtonClick(char _clickedLetter, Button _clickedButton)
         {
-            userInput += clickedLetter; // Add clicked letter to the user's input
+            clickedLetter = _clickedLetter;
+            userInput += _clickedLetter; // Add clicked letter to the user's input
             Debug.Log("Current User Input: " + userInput);
+            nameText.text = userInput;
 
             // Check if the user input matches the fruit name
             if (userInput.Equals(fruitName, StringComparison.OrdinalIgnoreCase)) // Case insensitive comparison
             {
                 Debug.Log("Congratulations! You've spelled the fruit name.");
                 // Perform actions for correct spelling
+                SpawnQuestion();
+                nameText.text = "";
+                playerScore++;
+            }
+
+        }
+
+        // Method to handle the "Undo" button click
+        public void UndoButtonClick()
+        {
+            if (userInput.Length > 0)
+            {
+                userInput = "";
+                nameText.text = userInput;
+
+                foreach (var buttonGO in buttonList)
+                {
+                    Button button = buttonGO.GetComponent<Button>();
+                    button.interactable = true;
+                }
+
             }
         }
 
@@ -94,6 +143,7 @@ namespace IQuiz
 
         #endregion
 
+        #region Questions Method
         public void SpawnQuestion()
         {
             if (fruitsGameObj != null)
@@ -116,5 +166,34 @@ namespace IQuiz
             string shuffledName = shuffler.ShuffleString(fruitName);
             CreateButtons(shuffledName);
         }
+        #endregion
+
+        #region Timer Method
+        public void UpdateTime()
+        {
+            if (gameTimer > 0)
+            {
+                gameTimer -= Time.deltaTime;
+
+                if (gameTimer < 5f && !timerSounds.isPlaying)
+                {
+                    timerSounds.Play();
+                }
+                if (gameTimer < 2f && timerSounds.isPlaying)
+                {
+                    timerSounds.Stop();
+                }
+            }
+
+        }
+
+        void OnRoundEnd(int round)
+        {
+            SpawnQuestion();
+            gameTimer = 15f;
+        }
+        #endregion
+
+
     }
 }
